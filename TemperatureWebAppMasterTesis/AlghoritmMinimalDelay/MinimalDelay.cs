d876d19018ad251b36b8890e1e9d162f46d2f050
Gjorgji <Gjorgji Si>
2023-04-18 17:52:06 +0200

    add Cloud Server

--- TemperatureWebAppMasterTesis/AlghoritmMinimalDelay/MinimalDelay.cs
+++ TemperatureWebAppMasterTesis/AlghoritmMinimalDelay/MinimalDelay.cs
@@ -27,6 +27,17 @@ namespace TemperatureWebAppMasterTesis.AlghoritmMinimalDelay
                     var TotalDelayAverage = TotalDelay / 2;
                     Console.WriteLine("TimeRequest:"+timeReq.ToString("dd:MM:yyyy h:mm:ss fff") + "\n TimeResponce:" + timeRes.ToString("dd:MM:yyyy h:mm:ss fff") + "\n Delay Second:" + delaySeconds.ToString() +"\n Delay miliseconds:"+ delayMiliSecond.ToString()+"\n Total Delay in miliseconds:"+ TotalDelay.ToString()+ "\n Average Total Delay:"+TotalDelayAverage.ToString());
                 }
+                else if (server1.ServerPing("192.168.100.24", 7188))
+                {
+                    Client ServerClient = new Client("https://192.168.100.24:7188/api/Temperature");
+                    await ServerClient.GetTemperature();
+                    var timeRes = DateTime.Now;
+                    var delaySeconds = Math.Abs(timeRes.Second - timeReq.Second);
+                    var delayMiliSecond = Math.Abs(timeReq.Millisecond - timeRes.Millisecond);
+                    var TotalDelay = delaySeconds * 1000 + delayMiliSecond;
+                    var TotalDelayAverage = TotalDelay / 2;
+                    Console.WriteLine("TimeRequest:" + timeReq.ToString("dd:MM:yyyy h:mm:ss fff") + "\n TimeResponce:" + timeRes.ToString("dd:MM:yyyy h:mm:ss fff") + "\n Delay Second:" + delaySeconds.ToString() + "\n Delay miliseconds:" + delayMiliSecond.ToString() + "\n Total Delay in miliseconds:" + TotalDelay.ToString() + "\n Average Total Delay:" + TotalDelayAverage.ToString());
+                }
                 else if(server1.ServerPing("192.168.100.15", 443))
                     {
                     Client ServerClient = new Client("https://192.168.100.15/Temperature");
@@ -44,6 +55,56 @@ namespace TemperatureWebAppMasterTesis.AlghoritmMinimalDelay
 
                 Server server1 = new Server();
                 if (server1.ServerPing("192.168.100.15", 443))
+                {
+                    Client ServerClient = new Client("https://192.168.100.15/Temperature");
+                    await ServerClient.GetTemperature();
+                    var timeRes = DateTime.Now;
+                    var delaySeconds = Math.Abs(timeRes.Second - timeReq.Second);
+                    var delayMiliSecond = Math.Abs(timeReq.Millisecond - timeRes.Millisecond);
+                    var TotalDelay = delaySeconds * 1000 + delayMiliSecond;
+                    var TotalDelayAverage = TotalDelay / 2;
+                    Console.WriteLine("TimeRequest:" + timeReq.ToString("dd:MM:yyyy h:mm:ss fff") + "\n TimeResponce:" + timeRes.ToString("dd:MM:yyyy h:mm:ss fff") + "\n Delay Second:" + delaySeconds.ToString() + "\n Delay miliseconds:" + delayMiliSecond.ToString() + "\n Total Delay in miliseconds:" + TotalDelay.ToString() + "\n Average Total Delay:" + TotalDelayAverage.ToString());
+                }
+                else if (server1.ServerPing("192.168.100.24", 443))
+                {
+                    Client ServerClient = new Client("https://192.168.100.24/Temperature");
+                    await ServerClient.GetTemperature();
+                    var timeRes = DateTime.Now;
+                    var delaySeconds = Math.Abs(timeRes.Second - timeReq.Second);
+                    var delayMiliSecond = Math.Abs(timeReq.Millisecond - timeRes.Millisecond);
+                    var TotalDelay = delaySeconds * 1000 + delayMiliSecond;
+                    var TotalDelayAverage = TotalDelay / 2;
+                    Console.WriteLine("TimeRequest:" + timeReq.ToString("dd:MM:yyyy h:mm:ss fff") + "\n TimeResponce:" + timeRes.ToString("dd:MM:yyyy h:mm:ss fff") + "\n Delay Second:" + delaySeconds.ToString() + "\n Delay miliseconds:" + delayMiliSecond.ToString() + "\n Total Delay in miliseconds:" + TotalDelay.ToString() + "\n Average Total Delay:" + TotalDelayAverage.ToString());
+                }
+                else if (server1.ServerPing("192.168.100.24", 7188))
+                {
+                    Client ServerClient = new Client("https://192.168.100.24:7188/api/Temperature");
+                    await ServerClient.GetTemperature();
+                    var timeRes = DateTime.Now;
+                    var delaySeconds = Math.Abs(timeRes.Second - timeReq.Second);
+                    var delayMiliSecond = Math.Abs(timeReq.Millisecond - timeRes.Millisecond);
+                    var TotalDelay = delaySeconds * 1000 + delayMiliSecond;
+                    var TotalDelayAverage = TotalDelay / 2;
+                    Console.WriteLine("TimeRequest:" + timeReq.ToString("dd:MM:yyyy h:mm:ss fff") + "\n TimeResponce:" + timeRes.ToString("dd:MM:yyyy h:mm:ss fff") + "\n Delay Second:" + delaySeconds.ToString() + "\n Delay miliseconds:" + delayMiliSecond.ToString() + "\n Total Delay in miliseconds:" + TotalDelay.ToString() + "\n Average Total Delay:" + TotalDelayAverage.ToString());
+                }
+            }),
+            Task.Run(async () =>
+            {
+                var timeReq = DateTime.Now;
+
+                Server server1 = new Server();
+                if (server1.ServerPing("192.168.100.24", 7188))
+                {
+                    Client ServerClient = new Client("https://192.168.100.24:7188/api/Temperature");
+                    await ServerClient.GetTemperature();
+                    var timeRes = DateTime.Now;
+                    var delaySeconds = Math.Abs(timeRes.Second - timeReq.Second);
+                    var delayMiliSecond = Math.Abs(timeReq.Millisecond - timeRes.Millisecond);
+                    var TotalDelay = delaySeconds * 1000 + delayMiliSecond;
+                    var TotalDelayAverage = TotalDelay / 2;
+                    Console.WriteLine("TimeRequest:" + timeReq.ToString("dd:MM:yyyy h:mm:ss fff") + "\n TimeResponce:" + timeRes.ToString("dd:MM:yyyy h:mm:ss fff") + "\n Delay Second:" + delaySeconds.ToString() + "\n Delay miliseconds:" + delayMiliSecond.ToString() + "\n Total Delay in miliseconds:" + TotalDelay.ToString() + "\n Average Total Delay:" + TotalDelayAverage.ToString());
+                }
+                else if (server1.ServerPing("192.168.100.15", 443))
                 {
                     Client ServerClient = new Client("https://192.168.100.15/Temperature");
                     await ServerClient.GetTemperature();
